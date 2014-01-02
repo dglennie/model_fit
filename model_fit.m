f853333cf518630294ed25edb51f96a1687dcb3e
dglennie <diana.glennie@gmail.com>
2014-01-02 15:18:19 -0500

    Created model_fit function

---
+++ model_fit.m
@@ -0,0 +1,8 @@
+function [] = model_fit()
+%MODEL_FIT Summary of this function goes here
+%   Employs fitting algorithm and 1D DRS model to extract chromophore
+%   concentrations from total diffuse reflectance spectrum from skin
+
+
+end
+
