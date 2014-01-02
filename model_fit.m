b83f27f981356e3faadfcdda2b28662c2a755f6d
dglennie <diana.glennie@gmail.com>
2014-01-02 15:53:23 -0500

    Select folder, get filenames

--- model_fit.m
+++ model_fit.m
@@ -3,6 +3,19 @@ function [] = model_fit()
 %   Employs fitting algorithm and 1D DRS model to extract chromophore
 %   concentrations from total diffuse reflectance spectrum from skin
 
+[pathname, filenames] = select_folder;
+
 
 end
 
+function [pathname, strucell2] = select_folder
+
+folder_name = uigetdir;
+pathname = strcat(folder_name,'\');
+files = dir(folder_name);
+
+strucell = struct2cell(files);
+strucell1 = strucell(1,:);
+strucell2 = strucell1(3:numel(files));
+
+end
