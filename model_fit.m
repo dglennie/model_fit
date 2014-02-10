69b8ccdb03ffb3f27d29c8725ae3bdc6f243f891
dglennie <diana.glennie@gmail.com>
2014-02-10 15:21:59 -0500

    Cleaned up comments/formatting
    Next time, start from Rm* calc

--- model_fit.m
+++ model_fit.m
@@ -17,33 +17,30 @@ else
 end
 
 % For each file,
-% i=1;
-% %for i=1:length(filenames)
-%     current_filename = filenames{i};
-%     
-%     % Check if current file contains calibration or background measurement
-%     b = strfind(current_filename, 'black');
-%     w = strfind(current_filename, 'white');
-%     
-%     if isempty(b) && isempty(w) % If file doesn't contain cal or bg, continue
-%         % Step 2: Process the measured data in current file
-%         % Step 2.1: Retrieve the modified measured reflectance
+ i=1;
+for i=1:length(filenames)
+   current_filename = filenames{i};
+   
+   % Check if current file contains calibration or background measurement
+   b = strfind(current_filename, 'black');
+   w = strfind(current_filename, 'white');
+   
+   if isempty(b) && isempty(w) % If file doesn't contain cal or bg, continue
+       % Step 2: Process the measured data in current file
+       % Step 2.1: Retrieve the modified measured reflectance
 %         rmstar = calc_rmeas(pathname, current_filename);
-%         
-%         % Step 2.2: Correct for substitution error
-%         % Step 2.3: Determine uncertainty?
-%         
-%         % Step 3: Fit model to measured data
-%         % Step 3.1: Determine coefficients using 'lsqcurvefit'
-%         % Step 3.2: Determine confidence interval using 'nlparci'
-%     else
-%         % Skip this file
-%     end
-% %end
-% 
-% 
-% 
-% 
+       
+       % Step 2.2: Correct for substitution error
+       % Step 2.3: Determine uncertainty?
+       
+       % Step 3: Fit model to measured data
+       % Step 3.1: Determine coefficients using 'lsqcurvefit'
+       % Step 3.2: Determine confidence interval using 'nlparci'
+   else
+       % Skip this file
+   end
+end
+
 end
 
 function [pathcat, strucell2] = select_folder
@@ -83,7 +80,6 @@ whtdata = dlmread(filewht,'	', [19,0,2066,1]); % reads in the spectra values, ta
 intwht = dlmread(filewht,' ', [6,3,6,3]); % reads in the integration time, space delimited
 wht = (whtdata(:,2)/(intwht/1000))';
 wht2=wht(453:1069);
-
 
 end
 
