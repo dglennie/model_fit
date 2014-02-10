39008ca87f555f0728d05aeb6ff1efc39cccfeac
dglennie <diana.glennie@gmail.com>
2014-02-10 11:38:37 -0500

    Added commented steps, check for black/white files

--- model_fit.m
+++ model_fit.m
@@ -3,12 +3,41 @@ function [] = model_fit()
 %   Employs fitting algorithm and 1D DRS model to extract chromophore
 %   concentrations from total diffuse reflectance spectrum from skin
 
+% Step 1: Determine location of files and files to process
+
+% Step 1.1: Get list of files from selected folder
 [pathname, filenames] = select_folder;
+
+% For each file,
+i=1;
+%for i=1:length(filenames)
+    current_file=strcat(pathname,filenames{i});
+    
+    % check if current file contains calibration or background measurement
+    b = strfind(current_file, 'black');
+    w = strfind(current_file, 'white');
+    
+    if isempty(b) && isempty(w) % if file doesn't contain cal or bg, continue
+        % Step 2: Process the measured data in current file
+        % Step 2.1: Retrieve the modified measured reflectance
+        % Step 2.2: Correct for substitution error
+        % Step 2.3: Determine uncertainty?
+        
+        % Step 3: Fit model to measured data
+        % Step 3.1: Determine coefficients using 'lsqcurvefit'
+        % Step 3.2: Determine confidence interval using 'nlparci'
+    else
+        % Skip this file
+    end
+%end
+
+
 
 
 end
 
 function [pathcat, strucell2] = select_folder
+%SELECT_FOLDER Get a list of files from selected folder
 
 folder_name = uigetdir;
 pathcat = strcat(folder_name,'\');
