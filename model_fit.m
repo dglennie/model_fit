6b2ad221a79d3232ee371b875c3541d89a49efa6
dglennie <diana.glennie@gmail.com>
2014-04-07 10:39:49 -0400

    Updated in preparation for finalizing code

--- model_fit.m
+++ model_fit.m
@@ -16,7 +16,7 @@ else
 end
 
 load('sbse_coeffs.mat')
-load('model_params.mat')
+%load('model_params.mat') %need to revisit
 
 % For each file,
 
@@ -30,10 +30,10 @@ for i=1:1 %length(filenames)
    if isempty(b) && isempty(w) % If file doesn't contain cal or bg, continue
        % Step 2: Process the measured data in current file
        % Step 2.1: Retrieve the modified measured reflectance
-       rmstar = calc_rmeas(pathname, current_filename, black, white);
+       rmeas = calc_rmeas(pathname, current_filename, black, white);
        
        % Step 2.2: Correct for substitution error
-       rm = corr_sbse(rmstar, sbse_coeffs);
+       rsbse = corr_sbse(rmeas, sbse_coeffs);
        
        % Step 2.3: Determine uncertainty?
        
@@ -50,79 +50,79 @@ end
 
 end
 
-function [pathcat, strucell2] = select_folder
+function [pathname, filenames] = select_folder
 %SELECT_FOLDER Get a list of files from selected folder
 
 folder_name = uigetdir;
-pathcat = strcat(folder_name,'\');
+pathname = strcat(folder_name,'\');
 files = dir(folder_name);
 
 strucell = struct2cell(files);
 strucell1 = strucell(1,:);
-strucell2 = strucell1(3:numel(files));
+filenames = strucell1(3:numel(files));
 
 end
 
-function [wl2, blk2, wht2] = get_bgcal(path, files)
+function [lambda, black, white] = get_bgcal(pathname, filenames)
 % GET_BGCAL Extracts background and calibration spectra, and wavelength
 
 % Retrieve black spec
-fndblk = ones(1,length(files)) - double(cellfun('isempty',strfind(files, 'black')));
+fndblk = ones(1,length(filenames)) - double(cellfun('isempty',strfind(filenames, 'black')));
 indblk = find(fndblk);
-fileblk = strcat(path,files{indblk});
+fileblk = strcat(pathname,filenames{indblk});
 blkdata = dlmread(fileblk,'	', [19,0,2066,1]); % reads in the spectra values, tabs delimited
 intblk = dlmread(fileblk,' ', [6,3,6,3]); % reads in the integration time, space delimited
 blk = (blkdata(:,2)/(intblk/1000))';
-blk2=blk(453:1069);
+black=blk(453:1069);
 
 % Retreive lambda values (500-705)
 wl = blkdata(:,1)';
-wl2=wl(453:1069);
+lambda=wl(453:1069);
 
 % Retreive white spec
-fndwht = ones(1,length(files)) - double(cellfun('isempty',strfind(files, 'white')));
+fndwht = ones(1,length(filenames)) - double(cellfun('isempty',strfind(filenames, 'white')));
 indwht = find(fndwht);
-filewht = strcat(path,files{indwht});
+filewht = strcat(pathname,filenames{indwht});
 whtdata = dlmread(filewht,'	', [19,0,2066,1]); % reads in the spectra values, tabs delimited
 intwht = dlmread(filewht,' ', [6,3,6,3]); % reads in the integration time, space delimited
 wht = (whtdata(:,2)/(intwht/1000))';
-wht2=wht(453:1069);
+white=wht(453:1069);
 
 end
 
-function rmeas = calc_rmeas(path, file, bkg, cal)
+function rmeas = calc_rmeas(pathname, current_filename, black, white);
 % % CALC_RMEAS Retrieve the modified measured reflectance from Master.Scope
 % % files
 % 
-curfile = strcat(path,file);
+curfile = strcat(pathname,current_filename);
 data = dlmread(curfile,'	', [19,0,2066,1]); % reads in the spectra values, tabs delimited
 inttime = dlmread(curfile,' ', [6,3,6,3]); % reads in the integration time, space delimited
 spec = (data(:,2)/(inttime/1000))';
 spec2 = spec(453:1069);
-rmeas = (spec2 - bkg)./(cal - bkg);
+rmeas = (spec2 - black)./(white - black);
 
 end
 
-function rcorr = corr_sbse(rmeas, coeff)
+function rsbse = corr_sbse(rmeas, sbse_coeffs)
 
-rcorr = ((coeff(:,1).*rmeas' + coeff(:,2))./(rmeas' + coeff(:,3)))';
-
-end
-
-function rdscat = calc_rd(beta,params,musp)
-%RDSCAT Calculate diffuse reflectance with added scatter losses
-
-% Step 1: Calculate diffuse reflectance using model
-mua = beta(1)*beta(2)*params(1,:) + (beta(1)-beta(1)*beta(2))*params(2,:) + beta(3)*params(3,:) + beta(4)*params(4,:) + beta(5);
-
-mutp = mua + musp;
-mueff = sqrt(3.*mua.*mutp);
-D = 1./(3.*mutp);
-
-%A = 2.348; % for an nrel = 1.33 (for water)
-A = 2.745; % for an nrel = 1.4 (for tissue)
-
-rdcalc = musp./((mutp + mueff).*(1 + 2.*A.*D.*mueff));
-
+rsbse = ((sbse_coeffs(:,1).*rmeas' + sbse_coeffs(:,2))./(rmeas' + sbse_coeffs(:,3)))';
 
 end
+
+% function rdscat = calc_rd(beta,params,musp) %need to confirm
+% %RDSCAT Calculate diffuse reflectance with added scatter losses
+% 
+% % Step 1: Calculate diffuse reflectance using model
+% mua = beta(1)*beta(2)*params(1,:) + (beta(1)-beta(1)*beta(2))*params(2,:) + beta(3)*params(3,:) + beta(4)*params(4,:) + beta(5);
+% 
+% mutp = mua + musp;
+% mueff = sqrt(3.*mua.*mutp);
+% D = 1./(3.*mutp);
+% 
+% %A = 2.348; % for an nrel = 1.33 (for water)
+% A = 2.745; % for an nrel = 1.4 (for tissue)
+% 
+% rdcalc = musp./((mutp + mueff).*(1 + 2.*A.*D.*mueff));
+% 
+% 
+% end
